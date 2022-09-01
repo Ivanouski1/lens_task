9bae8a77cd1d4698b50e9bf7f10682af6fe966a6
Ivanouski1 <100915019+Ivanouski1@users.noreply.github.com>
2022-09-01 23:02:14 +0300

    Update mount.sh

--- nfs-server/mount.sh
+++ nfs-server/mount.sh
@@ -6,6 +6,6 @@ sudo chown -R ubuntu:ubuntu /home/k8s_share/
 sudo chmod 777 /home/k8s_share
 sudo apt-get install nfs-kernel-server -y
 sudo chmod 666 /etc/exports
-sudo echo /home/k8s_share *(rw,no_root_squash,insecure,async,no_subtree_check) >> /etc/exports
+sudo echo "/home/k8s_share *(rw,no_root_squash,insecure,async,no_subtree_check)" >> /etc/exports
 sudo exportfs -ra
 sudo systemctl restart nfs-kernel-server
